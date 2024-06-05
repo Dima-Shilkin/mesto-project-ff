37bc8b8cbcf00923868221badf2e61f5896a4598
Dima Shilkin <DimaSchilkin@yandex.ru>
2024-06-05 11:37:34 +0300

    Исправил замечания

--- scripts/index.js
+++ scripts/index.js
@@ -6,18 +6,19 @@ const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 function createCard(element, deleteCard) {
-  let cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const deleteButton = cardElement.querySelector('.card__delete-button');
+  const cardImage = cardElement.querySelector('.card__image');
   deleteButton.addEventListener('click', deleteCard);
-  cardElement.querySelector('.card__image').src = element.link;
-  cardElement.querySelector('.card__image').alt = element.name;
+  cardImage.src = element.link;
+  cardImage.alt = element.name;
   cardElement.querySelector('.card__title').textContent = element.name;
   return cardElement;
 };
 
 // @todo: Функция удаления карточки
 function deleteCard(evt) {
-  evt.target.closest('.places__item').remove();
+  evt.target.closest('.card').remove();
 }
 
 // @todo: Вывести карточки на страницу
